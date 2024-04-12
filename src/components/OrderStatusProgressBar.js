feb917bd505218abd60365a2ea5b21ffd8896014
Man Mohan Singh <98742502+ManMohanSingh031@users.noreply.github.com>
2024-04-12 20:50:55 +0530

    OrderStatusProgressBar fix stage

--- src/components/OrderStatusProgressBar.js
+++ src/components/OrderStatusProgressBar.js
@@ -6,13 +6,17 @@ import Stage from "./Stage";
 const OrderStatusProgressBar = ({ stages }) => {
   return (
     <div className="w-full md:w-[95%] h-40 bg-white shadow-lg p-8 m-8">
-      <Origin />
+      <div className="hidden sm:block">
+        <Origin />
+      </div>
       <div className="bg-green-700 rounded-lg p-1 mx-4 md:mx-64 relative">
         {stages.map((stage, index) => (
           <Stage stage={stage} index={index} stagesLength={stages.length} />
         ))}
       </div>
-     <Distination />
+      <div className="hidden sm:block">
+        <Distination />
+      </div>
     </div>
   );
 };
