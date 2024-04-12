9c2035affd53f92d700015587bc3cbea082ff8dc
Man Mohan Singh <98742502+ManMohanSingh031@users.noreply.github.com>
2024-04-12 20:12:57 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import OrderStatusProgressBar from './components/OrderStatusProgressBar';
-// import OrderDetails from './components/OrderDetails';
-// import Map from './components/Map';
+import OrderDetails from './components/OrderDetails';
+import Map from './components/Map';
 import { stages } from './utils/constant';
 
 const App = () => {
   return (
     <div className=" w-full h-screen">
     <OrderStatusProgressBar stages={stages} />
-    {/* <OrderDetails/> */}
-    {/* <Map /> */}
+    <OrderDetails/>
+    <Map />
   </div>
   );
 };
